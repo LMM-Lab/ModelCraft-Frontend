21e76b4697dcb5ab542abae9e86430ce941992d7
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-01-18 21:34:14 +0900

    Button一旦

--- src/component/Button/index.tsx
+++ src/component/Button/index.tsx
@@ -1,9 +1,16 @@
-const Button=()=>{
-  return(
-    <div>
-      <button>Button</button>
-    </div>
-  )
+import styled from "styled-components"
+
+type ButtonProps={
+  onChange:string
+  width:string
+  height:string
+  backgroundColor:string
 }
+
+const Button=styled.button<ButtonProps>`
+  width:${({width})=>width};
+  height:${({height})=>height};
+  background-color:${({backgroundColor})=>backgroundColor};
+`
 
 export default Button
