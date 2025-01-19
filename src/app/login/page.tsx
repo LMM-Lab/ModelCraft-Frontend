a0c202d84f4867fc2ce29ccd0af04a3741b68154
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-01-19 23:13:09 +0900

    useForm installed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,18 @@
 import Text from "@/component/common/Text";
 import styled from "styled-components";
 import Input from "@/component/common/Input";
+import Flex from "@/component/common/styles/Flex";
+import Button from "@/component/common/Button";
 
-const Login=()=>{
+const Login = () => {
   return (
     <div>
-        <Input variants="default" placeholder="please input your e-mail"/>
+      <Text variants="ExtraLarge" margin="5rem 0 0 10rem">Login</Text>
+      <Flex $flex_direction="column" $marginTop="10rem">
+        <Input variants="default" placeholder="please enter your e-mail" />
+        <Input $marginTop="1.5rem" variants="default" placeholder="please enter your password" />
+        <Button $marginTop="10rem" variants="Medium">login</Button>
+      </Flex>
     </div>
   )
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,27 @@
 import Text from "@/component/common/Text";
 import styled from "styled-components";
+import Input from "@/component/common/Input";
+import Flex from "@/component/common/styles/Flex";
+import Button from "@/component/common/Button";
+import { useForm } from "react-hook-form";
 
-const Register=()=>{
+type FormData={
+  name:string
+  email:string
+}
+
+const Register = () => {
+  const {register,handleSubmit,formState:{errors}}=useForm<FormData>()
+
+
   return (
     <div>
-      <Text variants="ExtraLarge">Register</Text>
+      <Text variants="ExtraLarge" margin="5rem 0 0 10rem">Register</Text>
+      <Flex $flex_direction="column" $marginTop="10rem">
+        <Input variants="default" placeholder="please enter your e-mail" />
+        <Input $marginTop="1.5rem" variants="default" placeholder="please enter your password" />
+        <Button $marginTop="10rem" variants="Medium">login</Button>
+      </Flex>
     </div>
   )
 
