0be881ee93a06dae2c342d3a9a8309be34379ac2
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-01-20 17:23:04 +0900

    login-page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,35 @@
+'use client'
 import Text from "@/component/common/Text";
 import styled from "styled-components";
 import Input from "@/component/common/Input";
 import Flex from "@/component/common/styles/Flex";
 import Button from "@/component/common/Button";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const Login = () => {
+
+  type FormData = {
+    mail: string
+    password: string
+  }
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+
+  const onSubmit:SubmitHandler<FormData>=(data)=>{
+    console.log(data)
+  }
+
   return (
     <div>
       <Text variants="ExtraLarge" margin="5rem 0 0 10rem">Login</Text>
-      <Flex $flex_direction="column" $marginTop="10rem">
-        <Input variants="default" placeholder="please enter your e-mail" />
-        <Input $marginTop="1.5rem" variants="default" placeholder="please enter your password" />
-        <Button $marginTop="10rem" variants="Medium">login</Button>
-      </Flex>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex $flex_direction="column" $marginTop="10rem">
+          <Input {...register('mail',{required:'please enter your e-mail'})} $textAlign="center" variants="default" placeholder="e-mail" />
+          {errors.mail && <Text color="red" variants="Medium">{errors.mail.message}</Text>}
+          <Input {...register('password',{required:'please enter your password'})} $textAlign="center" $marginTop="1.5rem" variants="default" placeholder="password" />
+          {errors.password && <Text color="red" variants="Medium">{errors.password.message}</Text>}
+          <Button $marginTop="10rem" variants="Medium">login</Button>
+        </Flex>
+      </form>
     </div>
   )
 
