92d04ae8a86424c8af0a7eba156608cd936d7c55
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-01-23 15:11:58 +0900

    DnD調整

--- src/app/classification/train/page.tsx
+++ src/app/classification/train/page.tsx
@@ -7,19 +7,21 @@ import AddLayer from "./AddLayer";
 import ModelSetting from "./ModelSetting";
 
 
-const Train=()=>{
+const Train = () => {
 
   return (
     <div>
       <Text variants="ExtraLarge">Train</Text>
-      <Flex $width="1000px" $height="360px" $backgroundColor="White" $borderRadius="15px" $margin="0 auto">
-        <Text variants="Medium">Model</Text>
-        <Flex $flex_direction="column" $justify_content="flex-start" $align_items="flex-start">
-          <AddLayer></AddLayer>
-          <ModelSetting></ModelSetting>
+      <Flex $width="90%" $height="360px" $flex_direction="column" $align_items="center" $backgroundColor="White" $borderRadius="15px" $margin="0 auto">
+        <Flex $justify_content="space-between" $align_items="center" $width="95%" $height="fit-content" $marginTop="2rem">
+          <Text variants="Medium">Model</Text>
+          <Flex $flex_direction="column" $justify_content="flex-start" $align_items="flex-start">
+            <AddLayer></AddLayer>
+            <ModelSetting></ModelSetting>
+          </Flex>
         </Flex>
+        <DnD></DnD>
       </Flex>
-      <DnD></DnD>
     </div>
   )
 }
--- src/component/crassificationTrain/DnD/dynamicDnD.tsx
+++ src/component/crassificationTrain/DnD/dynamicDnD.tsx
@@ -2,46 +2,58 @@
 import { DragEndEvent } from "@dnd-kit/core"
 import { arrayMove, SortableContext } from "@dnd-kit/sortable"
 import { DndContext } from "@dnd-kit/core"
-import { useState } from "react"
+import React, { useState } from "react"
 import Flex from "@/component/common/styles/Flex"
 import Sortable from "./Sortable";
 import Layer from "../Layer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const DynamicDnD=()=>{
-  const [items,setItems]=useState(
+const DynamicDnD = () => {
+  const [items, setItems] = useState(
     {
       cards: [
-        { id: '1', title: 'Card 1' },
-        { id: '2', title: 'Card 2' },
-        { id: '3', title: 'Card 3' },
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+        { id: '5' },
+        { id: '6' },
+        { id: '7' },
+        { id: '8' },
       ],
     },
   )
 
-const handleDragEnd=(event:DragEndEvent)=>{
-  const {active,over}=event;
-  if(!over) return;
-  if(active.id===over.id) return;
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (!over) return;
+    if (active.id === over.id) return;
 
-  const oldSortable=active.data.current?.sortable;
-  const newSortable=over.data.current?.sortable;
-  if(!oldSortable||!newSortable) return;
+    const oldSortable = active.data.current?.sortable;
+    const newSortable = over.data.current?.sortable;
+    if (!oldSortable || !newSortable) return;
 
-  setItems({
-    ...items,
-    cards:arrayMove(items.cards,oldSortable.index,newSortable.index)
-  })
-}
+    setItems({
+      ...items,
+      cards: arrayMove(items.cards, oldSortable.index, newSortable.index)
+    })
+  }
 
-  return(
-    <div>
+  return (
+    <div style={{width:'91.5rem'}}>
       <DndContext onDragEnd={handleDragEnd}>
         <SortableContext items={items.cards}>
           <Flex $flex_wrap="wrap">
-            {items.cards.map((item)=>(
-              <Sortable id={item.id} key={item.id}>
-                <Layer name="Affine" input="120×120×3" output="245×120×3"></Layer>
-              </Sortable>
+            {items.cards.map((item, index) => (
+              <Flex $align_items="center" key={item.id}>
+                <Sortable id={item.id}>
+                  <Layer name="Affine" input="120×120×3" output="245×120×3" />
+                </Sortable>
+                {index < items.cards.length - 1 && !(index === 4) && (
+                  <FontAwesomeIcon icon={faAngleRight} style={{ fontSize: '2rem', margin: '0 0.8rem' }} />
+                )}
+              </Flex>
             ))}
           </Flex>
         </SortableContext>
