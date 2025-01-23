24b5021e904a19b608fd226e7c626e489605de7e
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-01-23 15:25:02 +0900

    フォルダ整理

--- src/app/classification/train/page.tsx
+++ src/app/classification/train/page.tsx
@@ -1,10 +1,11 @@
 import Button from "@/component/common/Button";
 import Flex from "@/component/common/styles/Flex";
 import Text from "@/component/common/Text";
-import DnD from "@/component/crassificationTrain/DnD";
+import DnD from "@/app/classification/train/Model/DnD";
 import styled from "styled-components";
-import AddLayer from "./AddLayer";
-import ModelSetting from "./ModelSetting";
+// import AddLayer from "./AddLayer";
+// import ModelSetting from "./Model/ModelSetting";
+import Model from "./Model";
 
 
 const Train = () => {
@@ -12,16 +13,7 @@ const Train = () => {
   return (
     <div>
       <Text variants="ExtraLarge">Train</Text>
-      <Flex $width="90%" $height="360px" $flex_direction="column" $align_items="center" $backgroundColor="White" $borderRadius="15px" $margin="0 auto">
-        <Flex $justify_content="space-between" $align_items="center" $width="95%" $height="fit-content" $marginTop="2rem">
-          <Text variants="Medium">Model</Text>
-          <Flex $flex_direction="column" $justify_content="flex-start" $align_items="flex-start">
-            <AddLayer></AddLayer>
-            <ModelSetting></ModelSetting>
-          </Flex>
-        </Flex>
-        <DnD></DnD>
-      </Flex>
+      <Model></Model>
     </div>
   )
 }
