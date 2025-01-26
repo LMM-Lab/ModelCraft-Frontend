c78e8fbb36771b609d588ca7a639dd9601dc3842
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-01-26 22:08:32 +0900

    <feature>(Train,Context-folder):ModelConfigを保存するためのコンテキストを追加 -コンテキストファイルを追加 -useModelConfigを追加

--- src/app/classification/train/page.tsx
+++ src/app/classification/train/page.tsx
@@ -1,38 +1,29 @@
 'use client'
 import Model from "./Model";
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
 import Progress from "./Progress";
 import Flex from "@/component/common/styles/Flex";
 import { paramsProps } from "./Model/types";
+import { ModelConfigContext, ModelConfigType } from "@/Context/ModelConfig";
 
-type GlobalStateType = {
-  state: number[];
-  setState: React.Dispatch<React.SetStateAction<number[]>>;
-};
-
-const GlobalStateContext = createContext<GlobalStateType | undefined>(undefined);
-
-export const useGlobalState = () => {
-  const context = useContext(GlobalStateContext);
-  if (!context) {
-    throw new Error("useGlobalState must be used within a GlobalStateProvider");
-  }
-  return context;
-};
 
 const Train = () => {
-  const [params,setParams]=useState<paramsProps[]>([]);
-  const [state, setState] = useState<number[]>([108]);
-  const [inputSize,setInputSize]=useState<number[]>([10])
+  const [params, setParams] = useState<paramsProps[]>([]);
+  const [modelConfig, setModelConfig] = useState<ModelConfigType>()
+  const [inputSize, setInputSize] = useState<number[]>([10])
+
+  useEffect(()=>{
+    console.log('modelConfig:',modelConfig)
+  },[modelConfig])
   return (
-    <Flex $flex_direction="column" $align_items="center">
-      <GlobalStateContext.Provider value={{ state, setState }}>
+    <ModelConfigContext.Provider value={{ modelConfig, setModelConfig }}>
+      <Flex $flex_direction="column" $align_items="center">
         <Model inputSize={inputSize} params={params} setParams={setParams}></Model>
-      </GlobalStateContext.Provider>
-      <Flex $justify_content="space-between" $marginTop="6rem">
-        <Progress></Progress>
+        <Flex $justify_content="space-between" $marginTop="6rem">
+          <Progress></Progress>
+        </Flex>
       </Flex>
-    </Flex>
+    </ModelConfigContext.Provider>
   )
 }
 
