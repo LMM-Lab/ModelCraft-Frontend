ae4aa83ec0d251ab018e2505c045bd851582c643
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-01-28 14:24:35 +0900

    <fix>(Train,Model):Trainのレンダリング中のModel以下での状態更新 を防止 >> -stateのparamsの定義をTrainからModelに変更

--- src/app/classification/train/page.tsx
+++ src/app/classification/train/page.tsx
@@ -1,20 +1,18 @@
 'use client'
 import Model from "./Model";
-import React, { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Progress from "./Progress";
 import Flex from "@/component/common/styles/Flex";
-import { paramsProps } from "./Model/types";
 import { ModelConfigContext, ModelConfigType } from "@/Context/ModelConfig";
 import Output from "./Output";
 
 const Train = () => {
-  const [params, setParams] = useState<paramsProps[]>([]);
   const [modelConfig, setModelConfig] = useState<ModelConfigType>()
   const [inputSize, setInputSize] = useState<number[]>([10])
   return (
     <ModelConfigContext.Provider value={{ modelConfig, setModelConfig }}>
       <Flex $flex_direction="column" $align_items="center">
-        <Model inputSize={inputSize} params={params} setParams={setParams}></Model>
+        <Model inputSize={inputSize}></Model>
         <Flex $justify_content="space-between" $marginTop="6rem">
           <Progress></Progress>
         </Flex>
