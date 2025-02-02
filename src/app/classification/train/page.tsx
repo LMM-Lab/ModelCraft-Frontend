bdd55106cab60102b19fd01eb8583cf7afa3c0cf
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-02-02 20:17:00 +0900

    (Train):paramsをTrainに移動

--- src/app/classification/train/page.tsx
+++ src/app/classification/train/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import Model from "./Model";
-import React, { useEffect, useState } from "react";
+import React, {  useMemo, useState } from "react";
 import Progress from "./Progress";
 import Flex from "@/component/common/styles/Flex";
 import { ModelConfigContext, ModelConfigType } from "@/Context/ModelConfig";
 import Output from "./Output";
 import Nav from "./Nav";
 import Data from "./Data";
+import { paramsProps } from "./Model/types";
 
 export type DataType={
   train?:File[]
@@ -17,12 +18,18 @@ export type DataType={
 const Train = () => {
   const [modelConfig, setModelConfig] = useState<ModelConfigType>()
   const [data,setData]=useState<DataType|null>(null)
+  const [params, setParams] = useState<paramsProps[]>([]);
+
+  const stableInputSize=useMemo(()=>{
+    return data?.inputSize ?? []
+  },[data?.inputSize])
+
   return (
     <ModelConfigContext.Provider value={{ modelConfig, setModelConfig }}>
       <Flex $flex_direction="column" $align_items="center">
         <Nav nav="train"></Nav>
         <Data setData={setData}></Data>
-        <Model inputSize={data?.inputSize ?? []}></Model>
+        <Model inputSize={stableInputSize} setParams={setParams} params={params}></Model>
         <Flex $justify_content="space-between" $marginTop="3rem">
           <Progress></Progress>
         </Flex>
