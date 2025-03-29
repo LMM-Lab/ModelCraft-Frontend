5a3efe9da80e96d362d521111a3da9693408c733
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-03-29 17:40:07 +0900

    <feat>(Progress):サーバーから受け取った進捗を表示

--- src/app/classification/train/Progress/ProgressBar.tsx
+++ src/app/classification/train/Progress/ProgressBar.tsx
@@ -9,13 +9,13 @@ const Div=styled.div<{$backgroundColor?:string}>`
   background-color:${({$backgroundColor})=>$backgroundColor};
 `
 
-const ProgressBar=({logs}:{logs:log[]})=>{
-  const epock=30
+const ProgressBar=({logs,maxEpoch}:{logs:log[],maxEpoch:number|undefined})=>{
+  const epock=maxEpoch ?? 0
   const epockArray=Array.from({length:epock},((_,index)=>index+1))
   return(
     <Flex $width="53rem" $margin="2rem 0 0 3rem">
       {epockArray.map((_,index)=>{
-        if(logs[index]?.epoch>index){
+        if(logs.length>index){
           return <Div key={index} $backgroundColor="#4F95D2"></Div>
         } else{
           return <Div key={index} $backgroundColor="#dbdbdb"></Div>
