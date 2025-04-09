089ab615410c9907fdcfdd4e39bc117026a045e8
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-04-09 21:04:02 +0900

    <fix>:Dilogコンポーネントを修正

--- src/component/common/Dialog/index.tsx
+++ src/component/common/Dialog/index.tsx
@@ -46,11 +46,12 @@ const DialogCSS = styled.dialog<DialogCSSProps>`
         return css`
           max-width:1000px;
           padding:2rem;
-          box-shadow: 0 0 50px rgba(0,0,0,0.5);
+          margin:0;
+          /* box-shadow: 0 0 50px rgba(0,0,0,0.5); */
           position:fixed;
           top:50%;
           left:50%;
-          transform:translate(-100%,-50%);
+          transform:translate(-50%,-50%);
         `
     }
   }}
