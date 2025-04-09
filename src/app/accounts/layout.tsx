e0692cf11d16b1da099947b7c032118732c3c5c4
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2025-04-09 20:50:34 +0900

    <feat>:acountsページセットアップ

---
+++ src/app/accounts/layout.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { ReactNode } from "react";
+import styled from "styled-components";
+import NavItem from "./NavItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faHexagonNodes } from "@fortawesome/free-solid-svg-icons";
+import Flex from "@/component/common/styles/Flex";
+
+const ContainerDiv = styled.div`
+  width:90rem;
+  height:90vh;
+  background-color:white;
+  border-radius:10px;
+  padding-top:8rem;
+  margin:3rem auto 0 auto;
+  display:flex;
+  justify-content:center;
+`
+
+const Line=styled.div`
+  border:1px solid #d0cece;
+  width:80%;
+  margin:3rem 0 1rem 0;
+`
+
+const fontSize='2.5rem'
+
+export default function Layout({ children }: { children: ReactNode }) {
+  if (!children) return null;
+  return (
+    <ContainerDiv>
+      <Flex $marginRight="3rem" $width="25%" $height="fit-content" $flex_direction="column" $justify_content="center" $align_items="center">
+        <NavItem href="/accounts/EditProfile" text="Edit profile"><FontAwesomeIcon icon={faCircleUser} style={{fontSize:fontSize}}/></NavItem>
+        <NavItem href="/accounts/Password" text="Password"><FontAwesomeIcon icon={faLock} style={{fontSize:fontSize}}/></NavItem>
+        <NavItem href="/accounts/ModelList" text="Model list"><FontAwesomeIcon icon={faHexagonNodes} style={{fontSize:fontSize}}/></NavItem>
+        <NavItem href="/accounts/Logout" text="Logout"><FontAwesomeIcon icon={faRightFromBracket} style={{fontSize:fontSize}}/></NavItem>
+        <Line></Line>
+        <NavItem href="/accounts/DeleteAccount" text="Delete account" color="#e20000"><FontAwesomeIcon icon={faXmark} style={{fontSize:'3.5rem',color:'#e20000'}}/></NavItem>
+      </Flex>
+      {children}
+    </ContainerDiv>
+  );
+}
